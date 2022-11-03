23f475ea08ee9157e920a74c974e30b1fd4710f7
saivikas-j <117098501+saivikas-j@users.noreply.github.com>
2022-11-03 12:55:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Simulation-based Measurements and Accuracy
-
 import matplotlib.pyplot as plt
 import simpy
 import random
@@ -22,65 +20,88 @@ time = []
 total_time = 0
 count = 0
 
-def plane_arrival(env,plane_arrival_rate,m,n):
-	while True:
-		capacity = random.randint(100,200)
-		foreigners = capacity // 3
-		locals = capacity - foreigners
-		for i in range(1,locals+1):
-			l = local(env,i,m)
-			env.process(l)
-		for j in range(1,foreigners+1):
-			f = foreign(env,j,n)
-			env.process(f)
-		inter_arrival_time = random.expovariate(plane_arrival_rate)
-		yield env.timeout(inter_arrival_time)
-
-def local(env,name,m):
+class Local():
+	def __init__(self, env, name, m):
+		self.env = env
+		self.name = name
+		self.m = m
+		self.walk_time = random.uniform(5,10)
+		self.check_time = random.uniform(0.5,1.0)
+		
+	def local(self):
 		global foreign_len,foreign_arr,local_len,local_arr,time,total_time,count
-		walk_time = random.uniform(5,10)
-		yield env.timeout(walk_time)
-		temp = env.now
+		yield env.timeout(self.walk_time)
+		temp = self.env.now
 		local_len = local_len + 1
 		local_arr.append(local_len)
 		foreign_arr.append(foreign_len)
-		time.append(env.now)
-		counter = m.request()
+		time.append(self.env.now)
+		counter = self.m.request()
 		yield counter
-		total_time = total_time + env.now - temp
-		check_time = random.uniform(0.5,1.0)
-		yield env.timeout(check_time)
+		total_time = total_time + self.env.now - temp		
+		yield self.env.timeout(self.check_time)
 		count = count + 1
-		m.release(counter)
+		self.m.release(counter)
 		local_len = local_len - 1
 		local_arr.append(local_len)
 		foreign_arr.append(foreign_len)
-		time.append(env.now)
-
-def foreign(env,name,n):
+		time.append(self.env.now)
+		
+class Foreigner:
+	def __init__(self, env, name, n):
+		self.env = env
+		self.name = name
+		self.n = n
+		self.walk_time = random.uniform(5,10)
+		self.check_time = random.uniform(2.0, 3.0)
+		
+	def foreign(self):
 		global foreign_len,foreign_arr,local_len,local_arr,time,total_time,count
-		walk_time = random.uniform(5,10)
-		yield env.timeout(walk_time)
-		temp = env.now
+		yield self.env.timeout(self.walk_time)
+		temp = self.env.now
 		foreign_len = foreign_len + 1
 		local_arr.append(local_len)
 		foreign_arr.append(foreign_len)
-		time.append(env.now)
-		counter = n.request()
+		time.append(self.env.now)
+		counter = self.n.request()
 		yield counter
-		total_time = total_time + env.now - temp
-		check_time = random.uniform(2.0,3.0)
-		yield env.timeout(check_time)
+		total_time = total_time + self.env.now - temp
+		yield self.env.timeout(self.check_time)
 		count = count + 1
-		n.release(counter)
+		self.n.release(counter)
 		foreign_len = foreign_len - 1
 		local_arr.append(local_len)
 		foreign_arr.append(foreign_len)
-		time.append(env.now)
+		time.append(self.env.now)
+
+		
+class Plane:
+	def __init__(self, name):
+		self.capacity = random.randint(100,200)
+		self.num_foreigner =  self.capacity // 3
+		self.num_local = self.capacity - self.num_foreigner
+		self.name = name
+
+def plane_arrival(env,plane_arrival_rate,m,n):
+	flag = 0
+	while True:
+		flag = flag + 1
+		p = Plane(flag)
+		for i in range(1, p.num_local + 1):
+			name = "Plane "+str(flag)+"-local "+str(i)
+			l = Local(env, name, m)
+			env.process(l.local())
+		for j in range(1, p.num_foreigner + 1):
+			name = "Plane "+str(flag)+"-foreigner "+str(j)
+			f = Foreigner(env, name, n)
+			env.process(f.foreign())
+		inter_arrival_time = random.expovariate(plane_arrival_rate)
+		yield env.timeout(inter_arrival_time)
 
 env = simpy.Environment()
-m = simpy.Resource(env, capacity=2)
-n = simpy.Resource(env, capacity=2)
+m = simpy.Resource(env, capacity = int(input("Enter number of local counters:")))
+n = simpy.Resource(env, capacity = int(input("Enter number of foreign counters:")))
+
 env.process(plane_arrival(env,plane_arrival_rate,m,n))
 
 env.run(until = 1440)
@@ -90,7 +111,7 @@ plt.plot(time,foreign_arr,label = "foreigners")
 plt.show()
 
 W = 0
-number = 100
+number = int(input("Enter number of simulations to be made: "))
 for i in range(0,number):
 	total_time = 0
 	count = 0
@@ -103,4 +124,3 @@ for i in range(0,number):
 
 W = W / number
 print("Average is",W)
-
